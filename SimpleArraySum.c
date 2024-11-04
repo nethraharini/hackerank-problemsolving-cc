3ed869c290358329d9a160d44438c2648d1643e9
Nethra Harini M <nethraharini0904@gmail.com>
2024-11-04 19:10:58 +0530

    SimpleArraySum.c

---
+++ SimpleArraySum.c
@@ -0,0 +1,15 @@
+int simpleArraySum(int ar_count, int* ar) {
+    int sum = 0;  // Initialize sum to zero
+
+    for (int i = 0; i < ar_count; i++) {
+        sum += ar[i];  // Add each element to sum
+    }
+
+    return sum; 
+    
+}
+
+//input
+//6
+// 1 2 3 4  10 11
+// 3`
